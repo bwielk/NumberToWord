75dbb7113567d15065a3270268e6c7bd3f3fb9f9
bwielk <bwielk@gmail.com>
2017-11-14 23:29:39 +0100

    Tens and above work

--- src/numToWords/Converter.java
+++ src/numToWords/Converter.java
@@ -6,11 +6,22 @@ public class Converter {
 		String result = "";
 		String units[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 		String teens[] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixtteen", "seventeen", "eighteen", "nineteen"};
+		String tens[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 		if(num <=9){
 			result = units[num];
 		}
 		else if(num >=10 && num <20){
 			result = teens[num-10];
+		}
+		else if(num >20 && num <=100){
+			if(num%10 != 0){
+				int ten = num/10;
+				int unit = num%10;
+				result = tens[ten] + " " + units[unit];
+			}else{
+				int ten = num/10;
+				result = tens[ten];
+			}
 		}
 		return result;
 	}
--- tests/numToWords/ConverterTest.java
+++ tests/numToWords/ConverterTest.java
@@ -38,5 +38,21 @@ public class ConverterTest {
 		int num = 19;
 		assertEquals("nineteen", converter.run(num));
 	}
+	
+	@Test
+	public void turn21intoTwentyOne(){
+		assertEquals("twenty one", converter.run(21));
+	}
+	
+	@Test
+	public void turns10sIntoTens(){
+		assertEquals("thirty", converter.run(30));
+		assertEquals("forty", converter.run(40));
+		assertEquals("fifty", converter.run(50));
+		assertEquals("sixty", converter.run(60));
+		assertEquals("seventy", converter.run(70));
+		assertEquals("eighty", converter.run(80));
+		assertEquals("ninety", converter.run(90));
+	}
 
 }
